8fa29a5ef5a8205a684192abf70ec603712a9919
cheolwook <loky123@naver.com>
2022-05-21 13:42:59 +0900

    Queue 사용 클래스 정의

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace QueueImplementation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random r = new Random();
+            MyQueue<float> que = new MyQueue<float>();
+
+            for (int i = 0; i < 5; i++)
+                que.EnQueue(new Node<float>(r.Next(100) / 100.0F));
+            que.Print();
+
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine("DeQueue : {0}", que.DeQueue());
+            que.Print();
         }
     }
 }
